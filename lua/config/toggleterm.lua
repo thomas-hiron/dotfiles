1c07a0d7e00d4a4a1a237d3a7f8875193993351a
Thomas Hiron <thomashiron@citia.org>
2024-02-13 10:40:54 +0100

    Remove ctrl+w mapping for toggleterm

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -7,7 +7,6 @@ function _G.set_terminal_keymaps()
 
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- Leave terminal mode
   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts) -- Return to previous buffer
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts) -- Handle Ctrl+W mapping
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
