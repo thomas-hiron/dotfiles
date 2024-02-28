c5f306e21cd523e997e4b12623f67dd7c0ba8525
Thomas Hiron <thomashiron@citia.org>
2024-02-28 11:21:13 +0100

    Update lualine to display buffers

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,11 +1,17 @@
 require ('lualine').setup {
   sections = {
-    lualine_c = {
+    lualine_a = {
       {
-        'filename',
-         path = 1,
+        'buffers',
+        show_modified_status = false,
+        max_length = vim.o.columns * 9 / 10,
       }
     },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {'encoding', 'fileformat', 'filetype'},
+    lualine_z = {},
   },
 }
 
