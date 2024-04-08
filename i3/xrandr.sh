73bf98cc8e7cfa2e56aa98d8c4e7299433fd22d7
Thomas Hiron <thomashiron@citia.org>
2024-04-08 08:55:47 +0200

    Configure i3 with 3 screens

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -8,4 +8,10 @@ if [[ ${monitors} == "eDP-1 HDMI-1" ]]; then
 
     i3-msg 'workspace 1; move workspace to output HDMI-1'
     i3-msg 'workspace 2; move workspace to output eDP-1'
+elif [[ ${monitors} == "eDP-1 DP-2-1 DP-2-2" ]]; then
+    echo "[INFO] Setting eDP-1 off and DP-2-2 right to DP-2-1"
+    xrandr --output eDP-1 --off --output DP-2-1 --auto --output DP-2-2 --auto --right-of DP-2-1
+
+    i3-msg 'workspace 1; move workspace to output DP-2-1'
+    i3-msg 'workspace 2; move workspace to output DP-2-2'
 fi
