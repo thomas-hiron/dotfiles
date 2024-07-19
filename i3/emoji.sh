0cbdbbe020a3d1eeaf6380c720d771a783223025
Thomas Hiron <thomashiron@citia.org>
2024-07-19 08:19:01 +0200

    Fix browser emoji typing

--- i3/emoji.sh
+++ i3/emoji.sh
@@ -4,4 +4,7 @@ emoji_file="/tmp/emoji.txt"
 test -f $emoji_file || curl -sL https://raw.githubusercontent.com/milesj/emojibase/master/packages/data/en/data.raw.json  | jq -r '.[] | .emoji + "  " + (.label | ascii_upcase) + "  " + (.tags | .? | map("#" + .) | join(" "))' > $emoji_file
 
 selected=$(cat $emoji_file | rofi -dmenu -i -p "Choose emoji" | grep -oP "^[^ ]+")
+
+# Fix web browser
+xdotool windowactivate $(xdotool getactivewindow)
 xdotool type $selected
