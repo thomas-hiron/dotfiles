b9c3f6cfdcc357c1e1802923e90a8feec9a3beb3
Thomas Hiron <thomashiron@citia.org>
2025-01-12 16:05:47 +0100

    Remove btc widget

--- polybar/btc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-btc=$(curl -Ls 'https://api.bitvavo.com/v2/ticker/24h' | jq -r '.[] | select(.market == "BTC-EUR").last')
-
-if [[ ! $btc =~ E\+ ]]; then
-    echo -n "BTC: ${btc} €"
-fi
