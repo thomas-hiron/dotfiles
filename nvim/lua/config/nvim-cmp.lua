0efae3a394c25146d27afb1e672680f256131a08
Thomas Hiron <thomashiron@citia.org>
2025-12-03 17:02:37 +0100

    Add cmp-buffer for opened buffers

--- nvim/lua/config/nvim-cmp.lua
+++ nvim/lua/config/nvim-cmp.lua
@@ -63,7 +63,14 @@ cmp.setup({
       { name = 'behat' },
       { name = 'html_class' },
       { name = 'path' },
-      { name = 'buffer' },
+      {
+        name = 'buffer',
+        option = {
+          get_bufnrs = function()
+            return vim.api.nvim_list_bufs()
+          end
+        }
+      },
       { name = 'form_options' },
       { name = 'symfony_routes' },
       { name = 'symfony_translations' },
